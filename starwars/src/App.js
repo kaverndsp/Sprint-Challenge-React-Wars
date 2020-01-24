fc0cc09ed0b606b96b9fa5921950f8b31596b96b
kaverndsp <kaverndsp@users.noreply.github.com>
2020-01-24 11:12:28 -0600

    data is now being sent to new component. working on adding in the cards

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,17 +1,23 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
+import Characters from "./components/Characters";
 import './App.css';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
-  const [people, setPeople] = useState();
+  const [people, setPeople] = useState([]);
 
-  axios.get('https://swapi.co/api/people/')
-  .then(response => {
-    console.log(response.data.results);
-  })
+    useEffect(() => {
+    axios.get("https://swapi.co/api/people/")
+    .then((response) => {
+      // console.log(response.data.results);
+        setPeople(response.data.results);
+    }).catch(error => {
+      console.log("The data could not be returned. ", error);
+    })
+}, [])
 
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
@@ -20,6 +26,12 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
+     
+      {people.map((people) => {
+         return (
+            <Characters people={people}/>
+          );
+        })}
     </div>
   );
 }
---
+++ starwars/src/components/Characters.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Characters(props) {
+
+    console.log(props);
+
+    return (
+        <div>
+            <h1>{props.people.name}</h1>
+                
+                
+            
+        </div>
+    );
+} 
+
+
+
+export default Characters;
